bad5fb75619b0c56c5883b4591d9e7f430551f79
quanlh87usa <quanlh87usa@gmail.com>
2026-05-16 02:33:13 -0400

    sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,11 +1,17 @@
-import requests
+""" Executing this function to analysis any text.
+"""
 import json
+import requests
+
 
 def sentiment_analyzer(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    '''do analysis a text input
+    '''
+    url = ('https://sn-watson-sentiment-bert.labs.skills.network/'
+            'v1/watson.runtime.nlp.v1/NlpService/SentimentPredict')
     myobj = { "raw_document": { "text": text_to_analyse }}
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
     if response.status_code == 200:
         # Parse the response from the API
         formatted_response = json.loads(response.text)
