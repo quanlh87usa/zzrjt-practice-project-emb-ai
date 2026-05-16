9c880a8fd3aa2430e966f15a5752f975ae9b734a
quanlh87usa <quanlh87usa@gmail.com>
2026-05-16 02:15:04 -0400

    commit

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = { "raw_document": { "text": text_to_analyse }}
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        label = None
+        score = None
+    # For any other unexpected status codes, set label and score to None
+    else:
+        label = None
+        score = None
+    return {'label': label, 'score': score}
